e0582bbaa7bb05abe75f4eb7e8865f3f7a06c7ff
eriDam <erika.c.caballero@gmail.com>
2014-11-02 13:11:55 +0100

    Act2c Menús Android
    complete the activity using textViews instead of ImageButton
    
    Signed-off-by: eriDam <erika.c.caballero@gmail.com>

--- Act2cMenusTxtview/src/com/example/act2cmenus/MainActivity.java
+++ Act2cMenusTxtview/src/com/example/act2cmenus/MainActivity.java
@@ -1,5 +1,10 @@
 package com.example.act2cmenus;
 
+/**
+ * @author Eri
+ * Actividad 2c Menús en android
+ * */
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.ContextMenu;
@@ -8,7 +13,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -17,23 +22,24 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		/**notes,box,camera...son los identificadores para asociar el menu contextual,
+		/**textVnotes,box,camera...son los identificadores para asociar el menu contextual,
 		 mediante su id*/
-		ImageButton ibtn = (ImageButton) findViewById(R.id.notes);
-//		ImageButton ibtb = (ImageButton) findViewById(R.id.box);
-//		ImageButton ibtc = (ImageButton) findViewById(R.id.camera);
-//		ImageButton ibtcal = (ImageButton) findViewById(R.id.cal);
-//		ImageButton ibtnf = (ImageButton) findViewById(R.id.nfile);
-//		ImageButton ibtnts = (ImageButton) findViewById(R.id.stadist);
+		TextView tvcNotes = (TextView) findViewById(R.id.textVnotes);
+		TextView tvcBox = (TextView) findViewById(R.id.textVbox);
+		TextView tvcCam = (TextView) findViewById(R.id.textVcam);
+		TextView tvcCalc = (TextView) findViewById(R.id.textVcalc);
+		TextView tvcFile = (TextView) findViewById(R.id.textVfile);
+		TextView tvcStadist = (TextView) findViewById(R.id.textVstadist);
 		/**
 		 * Asociamos el contextMenu(menú contextual) a los view(ibtn, etc..), 
 		 * */	
-    registerForContextMenu(ibtn);
-//	registerForContextMenu(ibtb);
-//	registerForContextMenu(ibtc);
-//	registerForContextMenu(ibtcal);
-//	registerForContextMenu(ibtnf);
-//	registerForContextMenu(ibtnts);
+		registerForContextMenu(tvcNotes);
+		registerForContextMenu(tvcBox);
+		registerForContextMenu(tvcCam);
+		registerForContextMenu(tvcCalc);
+		registerForContextMenu(tvcFile);
+		registerForContextMenu(tvcStadist);
+	
 	
 	}
 	/** Esta función es para crear el menu de opciones, utilizamos el inflater para instanciar
@@ -87,52 +93,54 @@ public class MainActivity extends Activity {
 		}
 	}
 	
-	/**Cuando pulsamos la ImageButton , nos saldrá un mensaje del context menu
+	/**Cuando pulsamos el Textview , nos saldrá un mensaje del context menu
 	 * procedemos de forma similar al menu del action bar (el options menu), utilizamos 
 	 * el inflater para instanciar ficheros xml de menús en objetos de menús, por parametro 
 	 * el xml donde esta definido el menu (el ContextMenuInfo)*/
-//	@Override
-//	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
-//		/**Llamamos al metodo heredado de la clase padre*/
-//		super.onCreateContextMenu(menu, v, menuInfo);
-//		/**Crear el menú, inflar el Layout, crear la apariencia del menu*/
-//		MenuInflater inflater = getMenuInflater();
-//		inflater.inflate(R.menu.context_menu, menu);
-//	}
-
-//	@Override
-//	public boolean onContextItemSelected(MenuItem item) {
-//		switch (item.getItemId()) {
-//		case R.id.notes:
-//			Toast.makeText(getApplicationContext(), "ContextMenu Shown",
-//					Toast.LENGTH_SHORT).show();
-//			return true;
-//	 
-//		case R.id.box:
-//			Toast.makeText(getApplicationContext(), "ContextMenu Shown",
-//					Toast.LENGTH_SHORT).show();
-//			return true;
-//			
-//		case R.id.camera:
-//			Toast.makeText(getApplicationContext(), "ContextMenu Shown",
-//					Toast.LENGTH_SHORT).show();
-//			return true;
-//			
-//		case R.id.cal:
-//			Toast.makeText(getApplicationContext(), "ContextMenu Shown",
-//					Toast.LENGTH_SHORT).show();
-//			return true;
-//			
-//		case R.id.nfile:
-//			Toast.makeText(getApplicationContext(), "ContextMenu Shown",
-//					Toast.LENGTH_SHORT).show();
-//			return true;
-//			
-//		case R.id.stadist:
-//			Toast.makeText(getApplicationContext(), "ContextMenu Shown",
-//					Toast.LENGTH_SHORT).show();
-//			return true;
-//		}
-//		return false;
-//	}
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+		/**Llamamos al metodo heredado de la clase padre*/
+		super.onCreateContextMenu(menu, v, menuInfo);
+	/**Crear el menú, inflar el Layout, crear la apariencia del menu*/
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.context_menu, menu);
+	}
+	
+	/**Cuando el item esté seleccionado, mostrará  un mensaje a parte de sus submenús*/
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.context_notes:
+			Toast.makeText(getApplicationContext(), "ContextMenu Notas",
+					Toast.LENGTH_SHORT).show();
+			return true;
+		case R.id.context_box:
+			Toast.makeText(getApplicationContext(), "ContextMenu caja",
+					Toast.LENGTH_SHORT).show();
+			return true;
+			
+		case R.id.context_camera:
+			Toast.makeText(getApplicationContext(), "ContextMenu Cámara",
+					Toast.LENGTH_SHORT).show();
+			return true;
+			
+		case R.id.context_cal:
+			Toast.makeText(getApplicationContext(), "ContextMenu Calculadora",
+					Toast.LENGTH_SHORT).show();
+			return true;
+			
+		case R.id.context_nfile:
+			Toast.makeText(getApplicationContext(), "ContextMenu Nuevo archivo",
+					Toast.LENGTH_SHORT).show();
+			return true;
+			
+		case R.id.context_stadist:
+			Toast.makeText(getApplicationContext(), "ContextMenu Estadísticas",
+					Toast.LENGTH_SHORT).show();
+			return true;
+		}
+		return false;
+	}
 }
+
+  
